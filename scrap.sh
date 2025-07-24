9b08ea83e8fba92dec1fe6121b862add5c9c3c77
Kazi Hossain <keh8423@nyu.edu>
2025-07-23 20:59:14 -0400

    Update scrap.sh
    Fixed redundant 2025 urls and descriptions. Updated the script to better filter new york

--- scrap.sh
+++ scrap.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # URL of the target website
-pitt="https://raw.githubusercontent.com/SimplifyJobs/Summer2025-Internships/dev/README.md"
-cvrve="https://raw.githubusercontent.com/cvrve/Summer2025-Internships/refs/heads/dev/README.md"
-speedySWE="https://raw.githubusercontent.com/speedyapply/2025-SWE-College-Jobs/refs/heads/main/README.md"
-speedyAI="https://raw.githubusercontent.com/speedyapply/2025-AI-College-Jobs/refs/heads/main/README.md"
+pitt="https://raw.githubusercontent.com/SimplifyJobs/Summer2026-Internships/dev/README.md"
+vansh="https://raw.githubusercontent.com/vanshb03/Summer2026-Internships/refs/heads/dev/README.md"
+speedySWE="https://raw.githubusercontent.com/speedyapply/2026-SWE-College-Jobs/refs/heads/main/README.md"
+speedyAI="https://raw.githubusercontent.com/speedyapply/2026-AI-College-Jobs/refs/heads/main/README.md"
 
 
 
 
 # Create Readme.md file and write the header
-echo "# Summer 2025 New York Tech Internships" > Readme.md
-echo "A fork of [Pitt CSC & Simplify](https://github.com/SimplifyJobs/Summer2025-Internships?tab=readme-ov-file), [cvrve Summer2025-Internships](https://github.com/cvrve/Summer2025-Internships), [speedyapply swe](https://github.com/speedyapply/2025-SWE-College-Jobs) & [speedyapply AI](https://github.com/speedyapply/2025-AI-College-Jobs)" >> Readme.md
+echo "# Curated list of Summer 2026 New York Tech Internships" > Readme.md
+echo "A fork of [Pitt CSC & Simplify](https://github.com/SimplifyJobs/Summer2026-Internships?tab=readme-ov-file), [cvrve Summer2026-Internships](https://github.com/vanshb03/Summer2026-Internships), [speedyapply swe](https://github.com/speedyapply/2026-SWE-College-Jobs) & [speedyapply AI](https://github.com/speedyapply/2026-AI-College-Jobs)" >> Readme.md
 echo "" >> Readme.md
-echo "Use this repository to share and keep track of software, tech, CS, PM, and quant internships for Summer 2025 in New York City." >> Readme.md
+echo "Use this repository to share and keep track of software, tech, CS, PM, and quant internships for Summer 2026 in New York City." >> Readme.md
 echo "" >> Readme.md
 echo "## The List 🚴🏔" >> Readme.md
 echo "### Legend" >> Readme.md
@@ -24,10 +24,10 @@ echo "| Company | Role | Location | Application/Link | Date Posted |" >> Readme.
 echo "| ------- | ---- | -------- | ---------------- | ----------- |" >> Readme.md
 
 # Append the filtered content to Readme.md
-curl -s "$pitt" | grep -E "NYC|NY" | grep -v "🔒" >> Readme.md
-curl -s "$cvrve" | grep -E "NYC|NY" | grep -v "🔒" >> Readme.md
-curl -s "$speedySWE" | grep -E "NYC|NY" >> Readme.md
-curl -s "$speedyAI" | grep -E "NYC|NY" >> Readme.md
+curl -s "$pitt" | grep -E "NYC|NY|New York" | grep -v "🔒" >> Readme.md
+curl -s "$cvrve" | grep -E "NYC|NY|New York" | grep -v "🔒" >> Readme.md
+curl -s "$speedySWE" | grep -E "NYC|NY|New York" >> Readme.md
+curl -s "$speedyAI" | grep -E "NYC|NY|New York" >> Readme.md
 
 
 
